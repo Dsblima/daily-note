a9988872d14cf02481d985ed969468025bf4f74b
Dsblima <dsbl@ecomp.poli.br>
2023-08-19 18:24:41 -0300

    Create IPC handler method

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,17 +1,70 @@
 import { IPC } from '@shared/constants/ipc'
-import { FetchAllDocumentsResponse } from '@shared/types/ipc'
+import {
+  CreateDocumentResponse,
+  DeleteDocumentRequest,
+  Document,
+  FetchAllDocumentsResponse,
+  FetchDocumentRequest,
+  FetchDocumentResponse,
+  SaveDocumentRequest,
+} from '@shared/types/ipc'
+import { randomUUID } from 'crypto'
 import { ipcMain } from 'electron'
+import { store } from './store'
 
 ipcMain.handle(
   IPC.DOCUMENTS.FETCH_ALL,
   async (): Promise<FetchAllDocumentsResponse> => {
     return {
-      data: [
-        { id: '1', title: 'Ignite', content: '' },
-        { id: '6', title: 'Web', content: '' },
-        { id: '5', title: 'Mobile', content: '' },
-        { id: '3', title: 'Mac', content: '' },
-      ],
+      data: Object.values(store.get('documents')),
     }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.FETCH,
+  async (_, { id }: FetchDocumentRequest): Promise<FetchDocumentResponse> => {
+    const document = store.get<string, Document>(`documents.${id}`)
+    return {
+      data: document,
+    }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.CREATE,
+  async (): Promise<CreateDocumentResponse> => {
+    const id = randomUUID()
+
+    const doc: Document = {
+      id,
+      title: 'untitled',
+    }
+
+    store.set(`documents.${id}`, doc)
+
+    return {
+      data: doc,
+    }
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.SAVE,
+  async (_, { id, title, content }: SaveDocumentRequest): Promise<void> => {
+    store.set(`documents.${id}`, {
+      id,
+      title,
+      content,
+    })
+  },
+)
+
+ipcMain.handle(
+  IPC.DOCUMENTS.DELETE,
+  async (_, { id }: DeleteDocumentRequest): Promise<void> => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore (https://github.com/sindresorhus/electron-store/issues/196)
+    store.delete(`documents.${id}`)
   },
 )
